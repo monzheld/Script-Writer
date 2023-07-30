3cf25cd755f713a90d7548b0b725d46eebc8e3ec
monzheld <monzheld@gmail.com>
2023-07-31 01:45:52 +0900

    deploy

--- script_app/__init__.py
+++ script_app/__init__.py
@@ -8,10 +8,10 @@ def index():
     return render_template('index.html')
 
 # script page
-@app.route('/script', methods=['GET'])
+@app.route('/script', methods=['POST'])
 def write_script():
     # input 값 받아오기 
-    file_type = request.args.get("file_type") 
+    file_type = request.form["file_type"]
     # {{ file_type1 }} : 총 문장 수 / {{ file_type2 }} : 최소 수정률 / {{ file_type3 }} : 수정해야 할 최소 문장 수 
     if file_type == "TS":
         file_type1 = "1,500"
@@ -22,13 +22,13 @@ def write_script():
         file_type2 = "90%"
         file_type3 = "900"
 
-    lang_pair = request.args.get("lang_pair")
-    token = request.args.get("token")
-    volume = request.args.get("volume", type=int)
-    unit_price = request.args.get("unit_price", type=int)
-    deadline = request.args.get("deadline")
-    reply_deadline = request.args.get("reply_deadline")
-    pm = request.args.get("pm")
+    lang_pair = request.form["lang_pair"]
+    token = request.form["token"]
+    volume = request.form["volume"]
+    unit_price = request.form["unit_price"]
+    deadline = request.form["deadline"]
+    reply_deadline = request.form["reply_deadline"]
+    pm = request.form["pm"]
 
     return render_template('result.html', 
                            file_type, file_type1, file_type2, file_type3, 
